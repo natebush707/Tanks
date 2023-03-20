8ee87f3c37a08ded08a1a5755641041ab9639153
Axel-LiraR <99221025+Axel-LiraR@users.noreply.github.com>
2023-03-19 22:01:10 -0700

    Level 1 - Need to test destructible wall w/ shell

--- Assets/Scripts/Shatter.cs
+++ Assets/Scripts/Shatter.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Shatter : MonoBehaviour
 {
+    // public ParticleSystem wallExplosion;
+    [SerializeField] ParticleSystem WallExplosion = null;
     public GameObject BrokenWall;
     void OnTriggerEnter(Collider other)
     {
-        // if (other.tag == "shell")
-        // {
-        //     Instantiate(BrokenWall, transform.position, transform.rotation);
-        //     Destroy(gameObject);
-        // }
-        Instantiate(BrokenWall, transform.position, transform.rotation);
-        Destroy(gameObject);
+        if (other.tag == "shell")
+        {
+            WallExplosion.Play();
+            // Instantiate(BrokenWall, transform.position, transform.rotation);
+            Destroy(gameObject, 1f);
+        }
+        // WallExplosion.Play();
+        // // Instantiate(BrokenWall, transform.position, transform.rotation);
+        // Destroy(gameObject, .75f);
     }
 }
