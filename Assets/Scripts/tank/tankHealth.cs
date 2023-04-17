8c54f6ea02c98de1214856583e8c325c22007e66
adrval <adrvalenz30@gmail.com>
2023-04-17 02:38:55 -0700

    ai tanks now keep distance properly, less spazzing out, dont kill player tank when defeated (rammers), and dissappear on death

--- Assets/Scripts/tank/tankHealth.cs
+++ Assets/Scripts/tank/tankHealth.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -55,7 +56,7 @@ public class tankHealth : MonoBehaviour
         if(other.tag == "enemyShell"){
             takeDamage(shellDamage);
         }
-        if(other.tag == "rammer")
+        if(other.tag == "rammer" && !other.gameObject.IsDestroyed())
         {
             onDeath();
         }
